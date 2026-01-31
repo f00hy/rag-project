ec6b16f07ab78fe9cdb512f90da0f6a1972d6020
f00hy <yaoheng20@gmail.com>
2026-02-01 02:05:47 +0800

    feat(infra): initialize relational database (supabase)

---
+++ app/infra/relational_db.py
@@ -0,0 +1,14 @@
+"""Database connection and initialization for Supabase."""
+
+from os import getenv
+from sqlmodel import create_engine, SQLModel
+from app.models import (
+    Document,
+)  # https://sqlmodel.tiangolo.com/tutorial/create-db-and-table/#sqlmodel-metadata-order-matters
+
+engine = create_engine(getenv("SUPABASE_URL"))
+
+
+def init_db():
+    """Create all database tables defined in SQLModel metadata."""
+    SQLModel.metadata.create_all(engine)
