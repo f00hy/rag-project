c3c075436ce1d937e67014b807746861da1e379f
f00hy <yaoheng20@gmail.com>
2026-02-03 18:38:14 +0800

    refactor(config): add type annotations

--- app/config.py
+++ app/config.py
@@ -1,11 +1,11 @@
 # Chunking
-PARENT_CHUNK_SIZE = 1024
-CHILD_CHUNK_SIZE = 256
-OVERLAP_CONTEXT_SIZE = 0.25
+PARENT_CHUNK_SIZE: int = 1024
+CHILD_CHUNK_SIZE: int = 256
+OVERLAP_CONTEXT_SIZE: float = 0.25
 
 # Embedding
-DENSE_MODEL_NAME = "jinaai/jina-embeddings-v3"
-SPARSE_MODEL_NAME = "prithivida/Splade_PP_en_v1"
+DENSE_MODEL_NAME: str = "jinaai/jina-embeddings-v3"
+SPARSE_MODEL_NAME: str = "prithivida/Splade_PP_en_v1"
 
 # Infra
-COLLECTION_NAME = "child_chunk"
+COLLECTION_NAME: str = "child_chunk"
