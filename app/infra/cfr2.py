2b2976f3b2d2b3828af4fa37b33bbe89cdda042b
f00hy <yaoheng20@gmail.com>
2026-02-08 02:41:05 +0800

    feat(infra): add cloudflare r2 storage client initialization

---
+++ app/infra/cfr2.py
@@ -0,0 +1,10 @@
+from os import getenv
+from boto3 import client
+
+client = client(
+    service_name="s3",
+    endpoint_url=f"https://{getenv('R2_ACCOUNT_ID')}.r2.cloudflarestorage.com",
+    aws_access_key_id=getenv("R2_ACCESS_KEY_ID"),
+    aws_secret_access_key=getenv("R2_SECRET_ACCESS_KEY"),
+    region_name="auto",
+)
