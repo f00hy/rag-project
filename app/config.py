df40053f801c0b5de908ee909208e3dd7e4f96fd
f00hy <yaoheng20@gmail.com>
2026-02-11 00:49:46 +0800

    perf(config): switch embedding model jina-embedding-v3 to bge-base-en-v1.5 to reduce memory usage
    the reduction in embedding token is acceptable since a child chunk is limited to 256 tokens

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ CHILD_CHUNK_SIZE: int = 256
 OVERLAP_CONTEXT_SIZE: float = 0.25
 
 # Embedding
-DENSE_MODEL_NAME: str = "jinaai/jina-embeddings-v3"
+DENSE_MODEL_NAME: str = "BAAI/bge-base-en-v1.5"
 SPARSE_MODEL_NAME: str = "prithivida/Splade_PP_en_v1"
 
 # Infrastructure
